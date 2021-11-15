9fcb539a6918aeb324eb3dcc3b9bc804643efab6
Ellen Sundholm <ellsu898@student.liu.se>
2021-11-15 16:10:40 +0100

    feat: add component Search for easier reuse of code
    * This component can be used for both city and country search pages

---
+++ components/Search.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
+import SearchButton from './SearchButton';
+
+/* 
+* Search component. Handle textinput on search pages.
+* Props:
+* incorrectInput:  showing if input is correct or not (boolean)
+* incorrectText: text to be shown if incorrect input (string)
+* placeholder: placeholder in TextInput (string
+* search: function for searching for the input ((input: string) => void)
+* setIncorrectInput: setter for incorrectInput  ((b: boolean) => void)
+*/
+
+type Props = {
+    incorrectInput: boolean,
+    placeholder: string,
+    incorrectText: string,
+    search: (input: string) => void,
+    setIncorrectInput: (b: boolean) => void
+}
+
+export default function Search({ incorrectInput, placeholder, incorrectText, search, setIncorrectInput }: Props) {
+
+    const [input, setInput] = useState("");
+
+    /* 
+     * Method for searching based in input state and resetting the input state to empty.
+     */
+    const searchInput = () => {
+        search(input)
+        setInput("")
+    }
+
+    return (
+        <View>
+            <View style={styles.textinputContainer}>
+                <TextInput value={input}
+                    onChangeText={(text) => {
+                        setInput(text)
+                        setIncorrectInput(false)
+                    }}
+                    style={styles.textinput}
+                    placeholder={placeholder} />
+            </View>
+            <SearchButton onPress={searchInput} />
+            {incorrectInput ?
+                <Text style={styles.incorrectText}>{incorrectText}</Text>
+                :
+                <View />
+            }
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+    textinput: {
+        backgroundColor: "#ffff",
+        padding: 10,
+        width: "100%",
+        textAlign: "center",
+    },
+    textinputContainer: {
+        alignItems: "center",
+        borderColor: "#2F4F4F",
+        borderWidth: 1
+    },
+    incorrectText: {
+        color: "#8b0000",
+        textAlign: "center"
+    }
+});
