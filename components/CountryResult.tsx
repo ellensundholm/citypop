08b8a95c31271321c09e40437dd9491547a16ee9
Ellen Sundholm <ellsu898@student.liu.se>
2021-11-15 13:14:13 +0100

    feat: design and functionality of component CountryResult
    * add design based on wireframes for result page after searching by country
    * add new component CityList, with list of cities
    * add method for navigating to CityResult when a city is pressed

--- components/CountryResult.tsx
+++ components/CountryResult.tsx
@@ -1,18 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/RootStackParamList'
+import CityList from './CityList';
 
 /**
- * Component for the page for the result after searching a country. The largest cities of this country will appear.
- */
+* Component for the page for the result after searching a country. The largest cities of this country will appear. 
+* Props: 
+* route: RouteProp<RootStackParamList, "CountryResult">
+* navigation: NativeStackNavigationProp<RootStackParamList, "CountryResult">
+*/
 
 type Props = NativeStackScreenProps<RootStackParamList, 'CountryResult'>;
 
 export default function CountryResult({ route, navigation }: Props) {
+    /**
+    * Function for navigating to the CityResult component when a city button in CityList is pressed.
+    */ 
+    const navigateToCity = (city: string, population: number) => {
+        navigation.navigate('CityResult', {city: city, population: population})
+    }
+    
     return (
-        <View>
-            <Text>countryresult</Text>
+        <View style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.text}>{route.params.country?.toUpperCase()}</Text>
+            </View>
+            <CityList cityList={route.params.cityList} toCity={navigateToCity}></CityList>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 15,
+        backgroundColor: "#8FBC8F",
+    },
+    title: {
+        fontSize: 32,
+    },
+    titleContainer: {
+        alignItems: "center",
+        marginBottom: 100,
+        marginTop: 50
+    },
+    text: {
+        fontWeight: "bold",
+        fontSize: 35,
+        color: "#2F4F4F",
+        textAlign: "center"
+    },
+});
