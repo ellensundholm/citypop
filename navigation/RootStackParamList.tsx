42b986224a6f4090423b954d7073e0702d45bceb
Ellen Sundholm <ellsu898@student.liu.se>
2021-11-15 12:12:20 +0100

    feat: rename City to CityResult and updated input props

--- navigation/RootStackParamList.tsx
+++ navigation/RootStackParamList.tsx
@@ -1,12 +1,12 @@
-import { CountryData } from '../components/CountryResult'
+import { CityData } from '../components/CountryList'
 
 
 type RootStackParamList = {
     HomeScreen: undefined;
     CitySearch: undefined;
     CountrySearch: undefined;
-    City: undefined;
-    CountryResult: { cityList: Array<CountryData>, country: string | undefined};
+    CityResult: { city: string, population: number};
+    CountryResult: { cityList: Array<CityData>, country: string | undefined};
 };
 
 
